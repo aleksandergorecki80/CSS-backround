01debb845006ad1da0182f756e25709ef9bc19ca
FoxCote <aleksandergorecki80@gmail.com>
2022-06-13 05:39:00 +0200

    Css png background on a linear gradient

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+var element = document.getElementById("foo");
+element.style.backgroundImage = "url(zawor.png)";
+
+
+window.addEventListener("resize", () => {
+    console.log(element)
+    var width = document.getElementById('foo').offsetWidth;
+    console.log(width)
+    element.style.backgroundSize = width / 2;
+}, true);

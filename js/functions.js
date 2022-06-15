5ec46dfba81fb52a494696a7a2aa540c88c41dc6
FoxCote <aleksandergorecki80@gmail.com>
2022-06-16 01:11:01 +0200

    JS some  events

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,16 @@ const result = liczby.map((x) => {
 })
 
 console.log(result)
+
+document.getElementById("back").addEventListener('click', (event) => {
+    if(event.target.className === "outside"){
+        console.log('DO YOUR JOB')
+    }
+});
+// Add event listener on keydown
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+  }, false);
